d1c27c020cc9a6311b969c0cdc97e78e567cfba6
MTPC-126 <mt_067@outlook.com>
2025-03-28 15:54:12 +0530

    Custom TreeView using Collection view

---
+++ src/UraniumUI.Material/Controls/CollectionTreeView.cs
@@ -0,0 +1,261 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows.Input;
+using UraniumUI.Extensions;
+using UraniumUI.Resources;
+
+namespace UraniumUI.Material.Controls
+{
+    public class CollectionTreeView : CollectionView
+    {
+        public static DataTemplate DefaultItemTemplate = new DataTemplate(() =>
+        {
+            var label = new Label { VerticalOptions = LayoutOptions.Center };
+            label.SetBinding(Label.TextProperty, new Binding("Name"));
+            return label;
+        });
+
+        public CollectionTreeView()
+        {
+            //BindableLayout.SetItemTemplate(this, new DataTemplate(() =>
+            //{
+            //    var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
+            //    holder.TreeView = this;
+            //    //holder.HeightRequest = 900;
+            //    // holder.MinimumHeightRequest = 500;
+            //    return holder;
+            //}));
+
+            this.ItemTemplate = new DataTemplate(() =>            
+            {
+                var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
+                holder.TreeView = this;
+                //holder.HeightRequest = 900;
+                // holder.MinimumHeightRequest = 500;
+                return holder;
+            });
+        }
+
+        private List<CollectionTreeViewNodeHolderView> GetChildViews()
+        {
+            var childViews = this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>().Where(x => x is CollectionTreeViewNodeHolderView).ToList();
+            return childViews;
+        }
+
+        protected override void OnHandlerChanged()
+        {
+            base.OnHandlerChanged();
+
+            if (SelectedItems is INotifyCollectionChanged observableSelectedItems)
+            {
+                if (Handler is null)
+                {
+                    observableSelectedItems.CollectionChanged -= SelectedItemsChanged;
+                }
+                else
+                {
+                    observableSelectedItems.CollectionChanged += SelectedItemsChanged;
+                }
+            }
+        }
+
+        // TODO: Remove default value and make default value as null in the next major version.
+        private BindingBase childrenBinding = new Binding("Children");
+
+        public BindingBase ChildrenBinding
+        {
+            get => childrenBinding; set
+            {
+                childrenBinding = value;                
+                foreach (CollectionTreeViewNodeHolderView view in GetChildViews())
+                {
+                    view.ChildrenBinding = value;
+                }
+            }
+        }
+
+        private string isExpandedPropertyName;
+
+        public string IsExpandedPropertyName
+        {
+            get => isExpandedPropertyName;
+            set
+            {
+                isExpandedPropertyName = value;
+                
+                foreach (CollectionTreeViewNodeHolderView view in GetChildViews())
+                {
+                    view.ApplyIsExpandedPropertyBindings();
+                }
+            }
+        }
+
+        private string isLeafPropertyName;
+
+        public string IsLeafPropertyName
+        {
+            get => isLeafPropertyName;
+            set
+            {
+                isLeafPropertyName = value;
+                foreach (CollectionTreeViewNodeHolderView view in GetChildViews())
+                {
+                    view.ApplyIsLeafPropertyBindings();
+                }
+            }
+        }
+
+        protected virtual void OnItemsSourceSet()
+        {
+            BindableLayout.SetItemsSource(this, ItemsSource);
+        }
+
+        private void OnItemTemplateChanged()
+        {
+            //BindableLayout.SetItemTemplate(this, new DataTemplate(() =>
+            //{
+            //    var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
+            //    holder.CollectionTreeView = this;
+            //    return holder;
+            //}));
+
+            this.ItemTemplate = new DataTemplate(() =>
+            {
+                var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
+                holder.TreeView = this;
+                //holder.HeightRequest = 900;
+                // holder.MinimumHeightRequest = 500;
+                return holder;
+            });
+
+            OnItemsSourceSet();
+        }
+
+        private void OnExpanderTemplateChanged()
+        {
+            // Same logic (for now)
+            OnItemTemplateChanged();
+        }
+
+        protected virtual void SelectedItemChanged()
+        {
+            if (SelectionMode == SelectionMode.None)
+            {
+                return;
+            }
+
+            foreach (var childHolder in GetChildViews())
+            {
+                childHolder.OnSelectedItemChanged();
+            }
+        }
+
+        protected virtual void OnSelectedItemsChanged(IList oldValue, IList newValue)
+        {
+            if (oldValue is INotifyCollectionChanged observableOld)
+            {
+                observableOld.CollectionChanged -= SelectedItemsChanged;
+            }
+
+            if (newValue is INotifyCollectionChanged observableCollectionNew)
+            {
+                observableCollectionNew.CollectionChanged += SelectedItemsChanged;
+            }
+
+            foreach (var childNode in this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>())
+            {
+                if (newValue.Contains(childNode.BindingContext) && !childNode.IsSelected)
+                {
+                    childNode.IsSelected = true;
+                }
+            }
+        }
+
+        private void SelectedItemsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    foreach (var item in e.NewItems)
+                    {
+                        var node = this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>().FirstOrDefault(x => x.BindingContext == item);
+                        if (node is not null && !node.IsSelected)
+                        {
+                            node.IsSelected = true;
+                        }
+                    }
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    foreach (var item in e.OldItems)
+                    {
+                        var node = this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>().FirstOrDefault(x => x.BindingContext == item);
+                        if (node is not null && node.IsSelected)
+                        {
+                            node.IsSelected = false;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public SelectionMode SelectionMode { get => (SelectionMode)GetValue(SelectionModeProperty); set => SetValue(SelectionModeProperty, value); }
+
+        public static readonly BindableProperty SelectionModeProperty = BindableProperty.Create(
+                   nameof(SelectionMode), typeof(SelectionMode), typeof(CollectionTreeView), SelectionMode.None);
+
+        public bool UseAnimation { get => (bool)GetValue(UseAnimationProperty); set => SetValue(UseAnimationProperty, value); }
+
+        public static readonly BindableProperty UseAnimationProperty = BindableProperty.Create(
+                   nameof(UseAnimation), typeof(bool), typeof(CollectionTreeView), true);
+
+        /// <summary>
+        /// Only indicates if CollectionTreeView is busy or not. Doesn't affect anything visually.
+        /// </summary>
+        public bool IsBusy { get; set; }
+
+        public IList ItemsSource { get => (IList)GetValue(ItemsSourceProperty); set => SetValue(ItemsSourceProperty, value); }
+
+        //public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+        //    nameof(ItemsSource), typeof(IList), typeof(CollectionTreeView), null,
+        //    propertyChanged: (b, o, v) => (b as CollectionTreeView).OnItemsSourceSet());
+
+        public object SelectedItem { get => GetValue(SelectedItemProperty); set => SetValue(SelectedItemProperty, value); }
+
+        public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(
+            nameof(SelectedItem), typeof(object), typeof(CollectionTreeView), default, BindingMode.TwoWay, propertyChanged: (bo, ov, nv) => (bo as CollectionTreeView).SelectedItemChanged());
+
+        public IList SelectedItems { get => (IList)GetValue(SelectedItemsProperty); set => SetValue(SelectedItemsProperty, value); }
+
+        public static readonly BindableProperty SelectedItemsProperty = BindableProperty.Create(
+            nameof(SelectedItems), typeof(IList), typeof(CollectionTreeView), defaultValueCreator: bindable => new ObservableCollection<object>(),
+            propertyChanged: (bo, ov, nv) => (bo as CollectionTreeView).OnSelectedItemsChanged((IList)ov, (IList)nv));
+
+        public DataTemplate ExpanderTemplate { get => (DataTemplate)GetValue(ExpanderTemplateProperty); set => SetValue(ExpanderTemplateProperty, value); }
+
+        public static readonly BindableProperty ExpanderTemplateProperty = BindableProperty.Create(
+            nameof(ExpanderTemplate), typeof(DataTemplate), typeof(CollectionTreeView), null,
+            propertyChanged: (b, o, v) => (b as CollectionTreeView).OnExpanderTemplateChanged());
+
+        public DataTemplate ItemTemplate { get => (DataTemplate)GetValue(ItemTemplateProperty); set => SetValue(ItemTemplateProperty, value); }
+
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
+            nameof(ItemTemplate), typeof(DataTemplate), typeof(CollectionTreeView),
+            defaultValue: DefaultItemTemplate, propertyChanged: (b, o, n) => (b as CollectionTreeView).OnItemTemplateChanged());
+
+        public ICommand LoadChildrenCommand { get => (ICommand)GetValue(LoadChildrenCommandProperty); set => SetValue(LoadChildrenCommandProperty, value); }
+
+        public static readonly BindableProperty LoadChildrenCommandProperty = BindableProperty.Create(
+            nameof(LoadChildrenCommand), typeof(ICommand), typeof(CollectionTreeView), null);
+
+        public Color SelectionColor { get => (Color)GetValue(SelectionColorProperty); set => SetValue(SelectionColorProperty, value); }
+
+        public static readonly BindableProperty SelectionColorProperty = BindableProperty.Create(
+            nameof(SelectionColor), typeof(Color), typeof(CollectionTreeView), ColorResource.GetColor("Secondary", "SecondaryDark", Colors.Pink));
+
+        public Brush SelectionBrush { get => (Brush)GetValue(SelectionBrushProperty); set => SetValue(SelectionBrushProperty, value); }
+        public static readonly BindableProperty SelectionBrushProperty = BindableProperty.Create(
+            nameof(SelectionBrush), typeof(Brush), typeof(CollectionTreeView), null);
+    }   
+}
---
+++ src/UraniumUI.Material/Controls/CollectionTreeViewNodeHolderView.cs
@@ -0,0 +1,462 @@
+﻿using InputKit.Shared.Helpers;
+using UraniumUI.Extensions;
+using UraniumUI.Pages;
+using UraniumUI.Triggers;
+using UraniumUI.Views;
+using static Microsoft.Maui.Controls.VisualStateManager;
+using Path = Microsoft.Maui.Controls.Shapes.Path;
+
+namespace UraniumUI.Material.Controls;
+
+[ContentProperty(nameof(NodeView))]
+public class CollectionTreeViewNodeHolderView : VerticalStackLayout
+{
+    public View NodeView { get => nodeContainer.Content; set => nodeContainer.Content = value; }
+
+    public CollectionTreeViewNodeHolderView ParentHolderView { get; private set; }
+
+    public CollectionTreeView TreeView { get; internal set; }
+
+    protected TreeViewNodeItemContentView nodeContainer = new TreeViewNodeItemContentView
+    {
+        //HorizontalOptions = LayoutOptions.Fill,
+        //HeightRequest = 150,
+    };
+
+    public VerticalStackLayout NodeChildren => nodeChildren;
+
+    internal protected VerticalStackLayout nodeChildren = new VerticalStackLayout
+    {
+        IsVisible = false
+    };
+
+    public DataTemplate DataTemplate { get; }
+
+    protected Grid rowStack = new Grid
+    {
+        ColumnDefinitions =
+        {
+            new ColumnDefinition(GridLength.Auto),
+            new ColumnDefinition(GridLength.Star),
+        }
+    };
+
+    private BindingBase childrenBinding;
+    public BindingBase ChildrenBinding
+    {
+        get => childrenBinding;
+        internal set
+        {
+            childrenBinding = value;
+            if (ChildrenBinding is not null)
+            {
+                nodeChildren.SetBinding(BindableLayout.ItemsSourceProperty, new Binding((ChildrenBinding as Binding)?.Path));
+            }
+            else
+            {
+                nodeChildren.RemoveBinding(BindableLayout.ItemsSourceProperty);
+            }
+        }
+    }
+
+    private protected View expanderView;
+    private protected ButtonView iconArrow;
+
+    public CollectionTreeViewNodeHolderView(DataTemplate dataTemplate, CollectionTreeView treeView, BindingBase childrenBinding, int indentLevel = 0)
+    {
+        if (treeView is null)
+        {
+            throw new ArgumentNullException(nameof(treeView));
+        }
+
+        TreeView = treeView;
+        DataTemplate = dataTemplate;
+
+        nodeContainer.ItemTemplate = DataTemplate;
+        nodeContainer.SetBinding(TreeViewNodeItemContentView.ItemProperty, ".");
+
+        expanderView = TreeView.ExpanderTemplate?.CreateContent() as View ?? InitializeArrowExpander();
+        expanderView.BindingContext = this;
+
+        //this.SetBinding(SpacingProperty, new Binding(nameof(TreeView.Spacing), source: treeView));
+        //nodeChildren.SetBinding(VerticalStackLayout.SpacingProperty, new Binding(nameof(TreeView.Spacing), source: treeView));
+
+        rowStack.Add(expanderView);
+        rowStack.Add(nodeContainer, column: 1);
+
+        var button = new StatefulContentView
+        {
+            Content = rowStack,
+            Padding = new Thickness(indentLevel * 16, 0, 0, 0),
+#if WINDOWS
+            GestureRecognizers = { new TapGestureRecognizer { Command = new Command(ItemClicked) } },
+#else
+            TappedCommand = new Command(ItemClicked),
+#endif
+        };
+
+        this.Add(button);
+        this.Add(nodeChildren);
+
+        if (!string.IsNullOrEmpty(TreeView.IsExpandedPropertyName))
+        {
+            this.SetBinding(IsExpandedProperty, new Binding(TreeView.IsExpandedPropertyName, BindingMode.TwoWay));
+        }
+
+        if (!string.IsNullOrEmpty(TreeView.IsLeafPropertyName))
+        {
+            this.SetBinding(IsLeafProperty, new Binding(TreeView.IsLeafPropertyName, BindingMode.TwoWay));
+        }
+
+        BindableLayout.SetItemTemplate(nodeChildren, new DataTemplate(() =>
+        {
+            var node = new CollectionTreeViewNodeHolderView(DataTemplate, TreeView, childrenBinding, indentLevel + 1);
+            node.ParentHolderView = this;
+            node.TreeView = TreeView;
+            return node;
+        }));
+
+        ChildrenBinding = childrenBinding;
+
+        nodeChildren.ChildAdded += (s, e) => OnPropertyChanged(nameof(IsLeaf));
+        nodeChildren.ChildRemoved += (s, e) => OnPropertyChanged(nameof(IsLeaf));
+    }
+
+    protected virtual View InitializeArrowExpander()
+    {
+        iconArrow = new ButtonView
+        {
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Start,
+            StyleClass = new[] { "TreeViewExpandButton" },
+            Padding = 0,
+            Margin = 0,
+#if WINDOWS
+            GestureRecognizers = { new TapGestureRecognizer { Command = new Command(() => IsExpanded = !IsExpanded) } },
+#else
+            PressedCommand = new Command(() => IsExpanded = !IsExpanded),
+#endif
+        };
+
+        iconArrow.Content = new ContentView
+        {
+            Margin = new Thickness(0, 0, 5, 0),
+            Content = new Path
+            {
+                StyleClass = new[] { "TreeView.Arrow" },
+                Data = UraniumShapes.ArrowRight,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+            }
+        };
+
+        iconArrow.Triggers.Add(new DataTrigger(typeof(View))
+        {
+            Binding = new Binding(nameof(IsLeaf)),
+            Value = true,
+            EnterActions =
+            {
+                new GenericTriggerAction<View>((view) =>
+                {
+                    view.Opacity = 0;
+                    view.InputTransparent = true;
+                })
+            },
+            ExitActions =
+            {
+                new GenericTriggerAction<View>((view) =>
+                {
+                    view.Opacity = 1;
+                    view.InputTransparent = false;
+                })
+            }
+        });
+
+        iconArrow.Triggers.Add(new DataTrigger(typeof(View))
+        {
+            Binding = new Binding(IsExpandedProperty.PropertyName),
+            Value = true,
+            EnterActions =
+            {
+                new GenericTriggerAction<View>((view) =>
+                {
+                    var rotation = 90;
+                    if (TreeView.UseAnimation)
+                    {
+                        view.RotateToSafely(rotation, 90, easing: Easing.BounceOut);
+                    }
+                    else
+                    {
+                        iconArrow.Rotation = rotation;
+                    }
+                })
+            },
+            ExitActions =
+            {
+                new GenericTriggerAction<ButtonView>((view) =>
+                {
+                    var rotation = 0;
+                    if (TreeView.UseAnimation)
+                    {
+                        view.RotateToSafely(rotation, 90, easing: Easing.BounceOut);
+                    }
+                    else
+                    {
+                        iconArrow.Rotation = rotation;
+                    }
+                })
+            }
+        });
+
+        return iconArrow;
+    }
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+
+        if (iconArrow is not null)
+        {
+            iconArrow.RotationY = this.IsRtl() ? 180 : 0;
+        }
+    }
+
+    protected override void OnBindingContextChanged()
+    {
+        base.OnBindingContextChanged();
+        OnSelectedItemChanged();
+    }
+
+    protected virtual void ItemClicked()
+    {
+        if (TreeView.SelectionMode == SelectionMode.Single)
+        {
+            if (TreeView.SelectedItem == BindingContext)
+            {
+                TreeView.SelectedItem = null;
+            }
+            else
+            {
+                TreeView.SelectedItem = BindingContext;
+            }
+        }
+        else if (TreeView.SelectionMode == SelectionMode.Multiple)
+        {
+            if (TreeView.SelectedItems.Contains(BindingContext))
+            {
+                TreeView.SelectedItems.Remove(BindingContext);
+            }
+            else
+            {
+                TreeView.SelectedItems.Add(BindingContext);
+            }
+        }
+    }
+
+    protected internal virtual void OnSelectedItemChanged()
+    {
+        if (TreeView.SelectionMode == SelectionMode.Single)
+        {
+            if (BindingContext == TreeView.SelectedItem)
+            {
+                IsSelected = !IsSelected;
+            }
+            else
+            {
+                IsSelected = false;
+            }
+
+            foreach (var childHolder in nodeChildren.Children.OfType<CollectionTreeViewNodeHolderView>())
+            {
+                childHolder.OnSelectedItemChanged();
+            }
+        }
+    }
+
+    protected virtual void IsSelectedChanged()
+    {
+        var button = this.FindInChildrenHierarchy<StatefulContentView>();
+
+        if (IsSelected)
+        {
+            VisualStateManager.GoToState(this, CommonStates.Selected);
+            if (TreeView.SelectionBrush is not null)
+            {
+                button.Background = TreeView.SelectionBrush;
+            }
+            else
+            {
+                button.BackgroundColor = TreeView.SelectionColor;
+            }
+
+            foreach (var item in button.FindManyInChildrenHierarchy<Path>())
+            {
+                item.StyleClass = new[] { "TreeView.Arrow.Selected" };
+            }
+
+            foreach (var item in button.FindManyInChildrenHierarchy<Label>())
+            {
+                item.StyleClass = new[] { "TreeView.Label.Selected" };
+            }
+        }
+        else
+        {
+            VisualStateManager.GoToState(button, CommonStates.Normal);
+            button.BackgroundColor = Colors.Transparent;
+            button.Background = Brush.Default;
+
+            foreach (var item in button.FindManyInChildrenHierarchy<Path>())
+            {
+                item.StyleClass = new[] { "TreeView.Arrow" };
+            }
+
+            foreach (var item in button.FindManyInChildrenHierarchy<Label>())
+            {
+                item.StyleClass = new[] { "TreeView.Label" };
+            }
+        }
+    }
+
+    protected virtual void AddSelectedResources(StatefulContentView button)
+    {
+        var surfaceColor = TreeView.SelectionColor.ToSurfaceColor();
+
+        button.Resources.Clear();
+        button.Resources.Add(new Style(typeof(Label))
+        {
+            CanCascade = true,
+            Setters =
+                {
+                    new Setter
+                    {
+                        Property = Label.TextColorProperty, Value = surfaceColor
+                    }
+                }
+        });
+
+        button.Resources.Add(new Style(typeof(Path))
+        {
+            CanCascade = true,
+            Setters =
+            {
+                new Setter
+                {
+                    Property = Path.FillProperty, Value = surfaceColor
+                }
+            }
+        });
+    }
+
+    public virtual void ApplyIsExpandedPropertyBindings()
+    {
+        if (TreeView.IsExpandedPropertyName is null)
+        {
+            this.RemoveBinding(IsExpandedProperty);
+        }
+        else
+        {
+            this.SetBinding(IsExpandedProperty, new Binding(TreeView.IsExpandedPropertyName, BindingMode.TwoWay));
+        }
+
+        foreach (CollectionTreeViewNodeHolderView item in Children.Where(x => x is CollectionTreeViewNodeHolderView))
+        {
+            item.ApplyIsExpandedPropertyBindings();
+        }
+    }
+
+    public virtual void ApplyIsLeafPropertyBindings()
+    {
+        if (TreeView.IsLeafPropertyName is null)
+        {
+            this.RemoveBinding(IsLeafProperty);
+        }
+        else
+        {
+            this.SetBinding(IsLeafProperty, new Binding(this.TreeView.IsLeafPropertyName, BindingMode.TwoWay));
+        }
+
+        foreach (CollectionTreeViewNodeHolderView item in Children.Where(x => x is CollectionTreeViewNodeHolderView))
+        {
+            item.ApplyIsLeafPropertyBindings();
+        }
+    }
+
+    protected internal virtual async void OnIsExpandedChanged(bool isExpanded)
+    {
+        if (isExpanded)
+        {
+            if (TreeView.UseAnimation)
+            {
+                nodeChildren.IsVisible = true;
+                nodeChildren.TranslateToSafely(0, 0, 50).FireAndForget();
+                nodeChildren.ScaleToSafely(1, 50).FireAndForget();
+                nodeChildren.FadeToSafely(1).FireAndForget();
+            }
+            else
+            {
+                nodeChildren.IsVisible = true;
+            }
+        }
+        else
+        {
+            if (TreeView.UseAnimation)
+            {
+                nodeChildren.TranslateToSafely(0, -nodeChildren.Height).FireAndForget();
+                nodeChildren.ScaleToSafely(0).FireAndForget();
+                nodeChildren.AnchorX = 0;
+                nodeChildren.AnchorY = 0;
+
+                await nodeChildren.FadeToSafely(0, 50);
+            }
+
+            nodeChildren.IsVisible = false;
+        }
+
+        LoadChildrenIfNecessary();
+    }
+
+    protected virtual void OnIsLeafChanged(bool? newValue)
+    {
+        if (newValue == true && IsExpanded)
+        {
+            IsExpanded = false;
+        }
+    }
+
+    protected virtual void LoadChildrenIfNecessary()
+    {
+        if (!IsLeaf && !NodeChildren.Children.Any()) // TODO: And children is empty
+        {
+            TreeView.LoadChildrenCommand?.Execute(this.BindingContext);
+        }
+    }
+
+    public bool IsLeaf
+    {
+        get => (bool?)GetValue(IsLeafProperty) ?? !nodeChildren.Children.Any();
+        set => SetValue(IsLeafProperty, value);
+    }
+
+    public static readonly BindableProperty IsLeafProperty = BindableProperty.Create(
+        nameof(IsLeaf), typeof(bool?), typeof(CollectionTreeViewNodeHolderView), null, BindingMode.TwoWay,
+        propertyChanged: (bindable, oldValue, newValue) => (bindable as CollectionTreeViewNodeHolderView).OnIsLeafChanged((bool?)newValue));
+
+    public bool IsExpanded { get => (bool)GetValue(IsExpandedProperty); set => SetValue(IsExpandedProperty, value); }
+
+    public static readonly BindableProperty IsExpandedProperty =
+        BindableProperty.Create(nameof(IsExpanded), typeof(bool), typeof(CollectionTreeViewNodeHolderView), false,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is CollectionTreeViewNodeHolderView holderView)
+                {
+                    holderView.OnPropertyChanged(nameof(IsExpanded));
+                    holderView.OnIsExpandedChanged((bool)newValue);
+                }
+            });
+
+    public bool IsSelected { get => (bool)GetValue(IsSelectedProperty); set => SetValue(IsSelectedProperty, value); }
+
+    public static readonly BindableProperty IsSelectedProperty = BindableProperty.Create(
+        nameof(IsSelected), typeof(bool), typeof(CollectionTreeViewNodeHolderView), false,
+            propertyChanged: (bindable, oldValue, newValue) => (bindable as CollectionTreeViewNodeHolderView).IsSelectedChanged());
+}

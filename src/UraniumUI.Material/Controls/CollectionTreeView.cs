bd297472f1e1e486fd7b7499b0683919294a08c5
Michael Ostermann <Michael.Ostermann@ITs4bm.com>
2025-03-30 23:40:52 +0200

    Refactor CollectionTreeView for improved functionality
    The `CollectionTreeView` class has been refactored to inherit from `ContentView`, introducing a new `rootView` for item layout management. The constructor initializes `rootView` with a vertical layout and sets the default item template. Key methods like `RegisterNode` and `GetChildViews` have been updated for better node management. The `OnItemTemplateChanged` method now includes robust handling for template updates. Selection logic has been simplified in `SelectedItemsChanged`, and several properties have been modernized with auto-implemented syntax. A new `ItemSpacing` property has been added for customization, and overall code structure has been cleaned up for better readability.

--- src/UraniumUI.Material/Controls/CollectionTreeView.cs
+++ src/UraniumUI.Material/Controls/CollectionTreeView.cs
@@ -8,7 +8,7 @@ using UraniumUI.Resources;
 
 namespace UraniumUI.Material.Controls
 {
-    public class CollectionTreeView : CollectionView
+    public class CollectionTreeView : ContentView
     {
         public static DataTemplate DefaultItemTemplate = new DataTemplate(() =>
         {
@@ -17,32 +17,40 @@ namespace UraniumUI.Material.Controls
             return label;
         });
 
+        private readonly List<CollectionTreeViewNodeHolderView> _registeredNodes = new();
+
+        internal void RegisterNode(CollectionTreeViewNodeHolderView node)
+        {
+            _registeredNodes.Add(node);
+        }
+
+        internal IEnumerable<CollectionTreeViewNodeHolderView> GetChildViewsOf(CollectionTreeViewNodeHolderView parent)
+        {
+            return _registeredNodes.Where(x => x.ParentHolderView == parent);
+        }
+
+        public IEnumerable<CollectionTreeViewNodeHolderView> AllNodeViews => _registeredNodes;
+
+        private readonly CollectionView rootView;
+        private bool isTemplateUpdating;
+        private DataTemplate nodeTemplate;
+
+
         public CollectionTreeView()
         {
-            //BindableLayout.SetItemTemplate(this, new DataTemplate(() =>
-            //{
-            //    var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
-            //    holder.TreeView = this;
-            //    //holder.HeightRequest = 900;
-            //    // holder.MinimumHeightRequest = 500;
-            //    return holder;
-            //}));
-
-            this.ItemTemplate = new DataTemplate(() =>            
+            rootView = new CollectionView
             {
-                var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
-                holder.TreeView = this;
-                //holder.HeightRequest = 900;
-                // holder.MinimumHeightRequest = 500;
-                return holder;
-            });
+                ItemsLayout = new LinearItemsLayout(ItemsLayoutOrientation.Vertical),
+                SelectionMode = SelectionMode.None
+            };
+
+            Content = rootView;
+
+            OnItemTemplateChanged(DefaultItemTemplate);
         }
 
-        private List<CollectionTreeViewNodeHolderView> GetChildViews()
-        {
-            var childViews = this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>().Where(x => x is CollectionTreeViewNodeHolderView).ToList();
-            return childViews;
-        }
+        private List<CollectionTreeViewNodeHolderView> GetChildViews() =>
+            _registeredNodes.ToList();
 
         protected override void OnHandlerChanged()
         {
@@ -51,24 +59,20 @@ namespace UraniumUI.Material.Controls
             if (SelectedItems is INotifyCollectionChanged observableSelectedItems)
             {
                 if (Handler is null)
-                {
                     observableSelectedItems.CollectionChanged -= SelectedItemsChanged;
-                }
                 else
-                {
                     observableSelectedItems.CollectionChanged += SelectedItemsChanged;
-                }
             }
         }
 
         // TODO: Remove default value and make default value as null in the next major version.
         private BindingBase childrenBinding = new Binding("Children");
-
         public BindingBase ChildrenBinding
         {
-            get => childrenBinding; set
+            get => childrenBinding;
+            set
             {
-                childrenBinding = value;                
+                childrenBinding = value;
                 foreach (CollectionTreeViewNodeHolderView view in GetChildViews())
                 {
                     view.ChildrenBinding = value;
@@ -77,14 +81,12 @@ namespace UraniumUI.Material.Controls
         }
 
         private string isExpandedPropertyName;
-
         public string IsExpandedPropertyName
         {
             get => isExpandedPropertyName;
             set
             {
                 isExpandedPropertyName = value;
-                
                 foreach (CollectionTreeViewNodeHolderView view in GetChildViews())
                 {
                     view.ApplyIsExpandedPropertyBindings();
@@ -93,7 +95,6 @@ namespace UraniumUI.Material.Controls
         }
 
         private string isLeafPropertyName;
-
         public string IsLeafPropertyName
         {
             get => isLeafPropertyName;
@@ -107,46 +108,43 @@ namespace UraniumUI.Material.Controls
             }
         }
 
-        protected virtual void OnItemsSourceSet()
+        private void OnItemTemplateChanged(DataTemplate newTemplate)
         {
-            BindableLayout.SetItemsSource(this, ItemsSource);
-        }
+            if (isTemplateUpdating || newTemplate == null)
+                return;
 
-        private void OnItemTemplateChanged()
-        {
-            //BindableLayout.SetItemTemplate(this, new DataTemplate(() =>
-            //{
-            //    var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
-            //    holder.CollectionTreeView = this;
-            //    return holder;
-            //}));
-
-            this.ItemTemplate = new DataTemplate(() =>
+            try
             {
-                var holder = new CollectionTreeViewNodeHolderView(ItemTemplate, this, ChildrenBinding);
-                holder.TreeView = this;
-                //holder.HeightRequest = 900;
-                // holder.MinimumHeightRequest = 500;
-                return holder;
-            });
+                isTemplateUpdating = true;
 
-            OnItemsSourceSet();
+                nodeTemplate = newTemplate;
+
+                rootView.ItemTemplate = new DataTemplate(() =>
+                {
+                    var holder = new CollectionTreeViewNodeHolderView(nodeTemplate, this, ChildrenBinding)
+                    {
+                        TreeView = this
+                    };
+                    return holder;
+                });
+            }
+            finally
+            {
+                isTemplateUpdating = false;
+            }
         }
 
         private void OnExpanderTemplateChanged()
         {
-            // Same logic (for now)
-            OnItemTemplateChanged();
+            OnItemTemplateChanged(ItemTemplate);
         }
 
         protected virtual void SelectedItemChanged()
         {
             if (SelectionMode == SelectionMode.None)
-            {
                 return;
-            }
 
-            foreach (var childHolder in GetChildViews())
+            foreach (CollectionTreeViewNodeHolderView childHolder in GetChildViews())
             {
                 childHolder.OnSelectedItemChanged();
             }
@@ -155,16 +153,12 @@ namespace UraniumUI.Material.Controls
         protected virtual void OnSelectedItemsChanged(IList oldValue, IList newValue)
         {
             if (oldValue is INotifyCollectionChanged observableOld)
-            {
                 observableOld.CollectionChanged -= SelectedItemsChanged;
-            }
 
-            if (newValue is INotifyCollectionChanged observableCollectionNew)
-            {
-                observableCollectionNew.CollectionChanged += SelectedItemsChanged;
-            }
+            if (newValue is INotifyCollectionChanged observableNew)
+                observableNew.CollectionChanged += SelectedItemsChanged;
 
-            foreach (var childNode in this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>())
+            foreach (CollectionTreeViewNodeHolderView childNode in _registeredNodes)
             {
                 if (newValue.Contains(childNode.BindingContext) && !childNode.IsSelected)
                 {
@@ -175,87 +169,141 @@ namespace UraniumUI.Material.Controls
 
         private void SelectedItemsChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            switch (e.Action)
+            if (e.Action == NotifyCollectionChangedAction.Add)
             {
-                case NotifyCollectionChangedAction.Add:
-                    foreach (var item in e.NewItems)
-                    {
-                        var node = this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>().FirstOrDefault(x => x.BindingContext == item);
-                        if (node is not null && !node.IsSelected)
-                        {
-                            node.IsSelected = true;
-                        }
-                    }
-                    break;
-                case NotifyCollectionChangedAction.Remove:
-                    foreach (var item in e.OldItems)
-                    {
-                        var node = this.FindManyInChildrenHierarchy<CollectionTreeViewNodeHolderView>().FirstOrDefault(x => x.BindingContext == item);
-                        if (node is not null && node.IsSelected)
-                        {
-                            node.IsSelected = false;
-                        }
-                    }
-                    break;
+                foreach (var item in e.NewItems)
+                {
+                    var node = _registeredNodes
+                        .FirstOrDefault(x => x.BindingContext == item);
+                    if (node != null && !node.IsSelected)
+                        node.IsSelected = true;
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (var item in e.OldItems)
+                {
+                    var node = _registeredNodes
+                        .FirstOrDefault(x => x.BindingContext == item);
+                    if (node != null && node.IsSelected)
+                        node.IsSelected = false;
+                }
             }
         }
 
-        public SelectionMode SelectionMode { get => (SelectionMode)GetValue(SelectionModeProperty); set => SetValue(SelectionModeProperty, value); }
+        public SelectionMode SelectionMode
+        {
+            get => (SelectionMode)GetValue(SelectionModeProperty);
+            set => SetValue(SelectionModeProperty, value);
+        }
 
         public static readonly BindableProperty SelectionModeProperty = BindableProperty.Create(
-                   nameof(SelectionMode), typeof(SelectionMode), typeof(CollectionTreeView), SelectionMode.None);
+            nameof(SelectionMode), typeof(SelectionMode), typeof(CollectionTreeView), SelectionMode.None);
 
-        public bool UseAnimation { get => (bool)GetValue(UseAnimationProperty); set => SetValue(UseAnimationProperty, value); }
+        public bool UseAnimation
+        {
+            get => (bool)GetValue(UseAnimationProperty);
+            set => SetValue(UseAnimationProperty, value);
+        }
 
         public static readonly BindableProperty UseAnimationProperty = BindableProperty.Create(
-                   nameof(UseAnimation), typeof(bool), typeof(CollectionTreeView), true);
+            nameof(UseAnimation), typeof(bool), typeof(CollectionTreeView), true);
 
-        /// <summary>
-        /// Only indicates if CollectionTreeView is busy or not. Doesn't affect anything visually.
-        /// </summary>
         public bool IsBusy { get; set; }
 
-        public IList ItemsSource { get => (IList)GetValue(ItemsSourceProperty); set => SetValue(ItemsSourceProperty, value); }
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
 
-        //public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
-        //    nameof(ItemsSource), typeof(IList), typeof(CollectionTreeView), null,
-        //    propertyChanged: (b, o, v) => (b as CollectionTreeView).OnItemsSourceSet());
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            nameof(ItemsSource), typeof(IEnumerable), typeof(CollectionTreeView),
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is CollectionTreeView tree)
+                {
+                    tree.rootView.ItemsSource = (IEnumerable)newValue;
+                }
+            });
 
-        public object SelectedItem { get => GetValue(SelectedItemProperty); set => SetValue(SelectedItemProperty, value); }
+        public object SelectedItem
+        {
+            get => GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
 
         public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(
-            nameof(SelectedItem), typeof(object), typeof(CollectionTreeView), default, BindingMode.TwoWay, propertyChanged: (bo, ov, nv) => (bo as CollectionTreeView).SelectedItemChanged());
+            nameof(SelectedItem), typeof(object), typeof(CollectionTreeView), default, BindingMode.TwoWay,
+            propertyChanged: (bo, ov, nv) => (bo as CollectionTreeView)?.SelectedItemChanged());
 
-        public IList SelectedItems { get => (IList)GetValue(SelectedItemsProperty); set => SetValue(SelectedItemsProperty, value); }
+        public IList SelectedItems
+        {
+            get => (IList)GetValue(SelectedItemsProperty);
+            set => SetValue(SelectedItemsProperty, value);
+        }
 
         public static readonly BindableProperty SelectedItemsProperty = BindableProperty.Create(
-            nameof(SelectedItems), typeof(IList), typeof(CollectionTreeView), defaultValueCreator: bindable => new ObservableCollection<object>(),
-            propertyChanged: (bo, ov, nv) => (bo as CollectionTreeView).OnSelectedItemsChanged((IList)ov, (IList)nv));
+            nameof(SelectedItems), typeof(IList), typeof(CollectionTreeView),
+            defaultValueCreator: bindable => new ObservableCollection<object>(),
+            propertyChanged: (bo, ov, nv) => (bo as CollectionTreeView)?.OnSelectedItemsChanged((IList)ov, (IList)nv));
 
-        public DataTemplate ExpanderTemplate { get => (DataTemplate)GetValue(ExpanderTemplateProperty); set => SetValue(ExpanderTemplateProperty, value); }
+        public DataTemplate ExpanderTemplate
+        {
+            get => (DataTemplate)GetValue(ExpanderTemplateProperty);
+            set => SetValue(ExpanderTemplateProperty, value);
+        }
 
         public static readonly BindableProperty ExpanderTemplateProperty = BindableProperty.Create(
             nameof(ExpanderTemplate), typeof(DataTemplate), typeof(CollectionTreeView), null,
-            propertyChanged: (b, o, v) => (b as CollectionTreeView).OnExpanderTemplateChanged());
+            propertyChanged: (b, o, v) => (b as CollectionTreeView)?.OnExpanderTemplateChanged());
 
-        public DataTemplate ItemTemplate { get => (DataTemplate)GetValue(ItemTemplateProperty); set => SetValue(ItemTemplateProperty, value); }
+        public DataTemplate ItemTemplate
+        {
+            get => (DataTemplate)GetValue(ItemTemplateProperty);
+            set => SetValue(ItemTemplateProperty, value);
+        }
 
         public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
             nameof(ItemTemplate), typeof(DataTemplate), typeof(CollectionTreeView),
-            defaultValue: DefaultItemTemplate, propertyChanged: (b, o, n) => (b as CollectionTreeView).OnItemTemplateChanged());
+            defaultValue: DefaultItemTemplate,
+            propertyChanged: (b, o, n) => (b as CollectionTreeView)?.OnItemTemplateChanged((DataTemplate)n));
 
-        public ICommand LoadChildrenCommand { get => (ICommand)GetValue(LoadChildrenCommandProperty); set => SetValue(LoadChildrenCommandProperty, value); }
+        public ICommand LoadChildrenCommand
+        {
+            get => (ICommand)GetValue(LoadChildrenCommandProperty);
+            set => SetValue(LoadChildrenCommandProperty, value);
+        }
 
         public static readonly BindableProperty LoadChildrenCommandProperty = BindableProperty.Create(
             nameof(LoadChildrenCommand), typeof(ICommand), typeof(CollectionTreeView), null);
 
-        public Color SelectionColor { get => (Color)GetValue(SelectionColorProperty); set => SetValue(SelectionColorProperty, value); }
+        public Color SelectionColor
+        {
+            get => (Color)GetValue(SelectionColorProperty);
+            set => SetValue(SelectionColorProperty, value);
+        }
 
         public static readonly BindableProperty SelectionColorProperty = BindableProperty.Create(
-            nameof(SelectionColor), typeof(Color), typeof(CollectionTreeView), ColorResource.GetColor("Secondary", "SecondaryDark", Colors.Pink));
+            nameof(SelectionColor), typeof(Color), typeof(CollectionTreeView),
+            ColorResource.GetColor("Secondary", "SecondaryDark", Colors.Pink));
 
-        public Brush SelectionBrush { get => (Brush)GetValue(SelectionBrushProperty); set => SetValue(SelectionBrushProperty, value); }
+        public Brush SelectionBrush
+        {
+            get => (Brush)GetValue(SelectionBrushProperty);
+            set => SetValue(SelectionBrushProperty, value);
+        }
+
         public static readonly BindableProperty SelectionBrushProperty = BindableProperty.Create(
             nameof(SelectionBrush), typeof(Brush), typeof(CollectionTreeView), null);
-    }   
-}
+
+        public double ItemSpacing
+        {
+            get => (double)GetValue(ItemSpacingProperty);
+            set => SetValue(ItemSpacingProperty, value);
+        }
+
+        public static readonly BindableProperty ItemSpacingProperty = BindableProperty.Create(
+            nameof(ItemSpacing), typeof(double), typeof(CollectionTreeView), 0.0);
+    }
+}  

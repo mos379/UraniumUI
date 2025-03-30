86c5fd55d60f5f125f9e1d52837b4c1732231d28
Michael Ostermann <Michael.Ostermann@ITs4bm.com>
2025-03-30 23:39:34 +0200

    Add ItemSpacingToItemsLayoutConverter class
    Introduces a new converter class `ItemSpacingToItemsLayoutConverter` in the `UraniumUI.Converters` namespace. Implements the `IValueConverter` interface with methods to convert item spacing to a `LinearItemsLayout` and back. Includes necessary using directives for system libraries.

---
+++ src/UraniumUI/Converters/ItemSpacingToItemsLayoutConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UraniumUI.Converters
+{
+    public class ItemSpacingToItemsLayoutConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double spacing = 0;
+
+            if (value is double s)
+                spacing = s;
+
+            return new LinearItemsLayout(ItemsLayoutOrientation.Vertical)
+            {
+                ItemSpacing = spacing
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is LinearItemsLayout layout)
+                return layout.ItemSpacing;
+
+            return 0;
+        }
+    }
+}
